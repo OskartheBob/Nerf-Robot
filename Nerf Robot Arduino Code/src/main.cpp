f1e1001cf87f51858902b4f4d844f6d7a3ffdca1
Oskar <oskarsokol.1@gmail.com>
2025-01-23 19:58:22 +0000

    DC motor addidtion

--- Nerf Robot Arduino Code/src/main.cpp
+++ Nerf Robot Arduino Code/src/main.cpp
@@ -6,11 +6,14 @@ Servo myservo;
 
 int potpin = 0;  
 int val;    
+int E2 = 5;
+int M2 = 4;
 
 void setup() {
   myservo.attach(9);  
   Serial.begin(115200);
   matrix.begin();
+  pinMode(M2, OUTPUT);
 }
 
 const uint32_t heart[] = {
@@ -25,6 +28,14 @@ void loop() {
   val = map(val, 0, 1023, 0, 300);     
   myservo.write(val);                  
   delay(15);                           
+
+int value;
+for(value = 0 ; value <= 255; value+=5)
+  {
+    digitalWrite(M2,HIGH);
+    analogWrite(E2, value);   //PWM Speed Control
+    delay(30);
+  }
 
   matrix.loadFrame(heart);
 
