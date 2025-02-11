7394fe7df84bf0387c1c192284c10fb38fced6a4
Oskar <oskarsokol.1@gmail.com>
2025-02-11 16:02:16 +0000

    Accelerometer working, Accelerometer smoothed, Servo controllable with Accelerometer

--- Nerf Robot Arduino Code/src/main.cpp
+++ Nerf Robot Arduino Code/src/main.cpp
@@ -3,47 +3,65 @@
 #include <Adafruit_Sensor.h> 
 #include <Wire.h>
 #include <SPI.h>
+#include <RunningAverage.h>
 
 Servo myservo;  
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+RunningAverage myRa1(16);
+RunningAverage myRa2(16);
 
-int potpin = 0;  
-int val;    
 int E2 = 5;
 int M2 = 4;
 int motor_button = 2;
 int motor_speed = 6;
 
+float average = 0;
+long degrees = 0;
+
+/*unsigned long previous_millis = 0;
+void set_interval(unsigned long interval, void (*callback)()) {
+  unsigned long current_millis = millis();
+  if(current_millis - previous_millis >= interval){
+    previous_millis = current_millis;
+    callback();
+  }
+}*/
+
+float mapfloat(float x, float in_min, float in_max, long out_min, long out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
 void setup() {
   myservo.attach(9);  
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(M2, OUTPUT);
   pinMode(motor_button, INPUT_PULLUP);
   pinMode(motor_speed, INPUT_PULLUP);
+
+  if (! mma.begin()) {
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  Serial.println("MMA8451 found!");
+  mma.setRange(MMA8451_RANGE_2_G);
 }
 
 
 
 void loop() {
-  val = analogRead(potpin);            
-  val = map(val, 0, 1023, 0, 300);     
-  myservo.write(val);                  
-  delay(15);                           
+  mma.read(); 
+  sensors_event_t event; 
+  mma.getEvent(&event);
 
-
-  if(digitalRead(motor_speed) == HIGH){
-    analogWrite(E2, 150);
-  }
-    else{
-      analogWrite(E2, 0);
-    }
-
-  digitalWrite(M2, LOW);
-  /*if(digitalRead(motor_button) == HIGH){
-    digitalWrite(M2, HIGH);
-  }
-    else{
-      digitalWrite(M2, LOW);
-    }*/
-  
-
+  myRa1.addValue(event.acceleration.x);
+  average = myRa1.getAverage();
+  average = mapfloat(average, -10, 10, 0, 180);
+  myRa2.addValue(average);
+  degrees = myRa2.getAverage();
+  Serial.println(degrees);
+    
+  myservo.write(degrees); 
+  delay(15);
 }
